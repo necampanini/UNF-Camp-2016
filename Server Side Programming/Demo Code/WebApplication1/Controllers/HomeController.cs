acab4dad2ce315f53d9c6ffe90d7a14ab5fd6774
Paul Irwin <paulirwin@gmail.com>
2016-06-18 12:02:42 -0400

    Add presentation and demo code for server-side programming

---
+++ Server Side Programming/Demo Code/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<BlogEntry> entries = new List<BlogEntry>()
+        {
+            new BlogEntry() {
+                Id = 1,
+                Title = "Test 1",
+                Body = "This is the 1st blog entry"
+            },
+            new BlogEntry() {
+                Id = 2,
+                Title = "Test 2",
+                Body = "This is the 2nd blog entry"
+            }
+        };
+
+        public ActionResult Index()
+        {
+            return View(entries);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var entry = entries.FirstOrDefault(i => i.Id == id);
+
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(entry);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(BlogEntry model)
+        {
+            model.Id = entries.Count + 1;
+
+            entries.Add(model);
+
+            return Redirect("/");
+        }
+    }
+}
+
+
+
+
+
