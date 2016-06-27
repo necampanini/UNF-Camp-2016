7fb9a509890028390805a99df33d71968700e769
Joseph P. Short <josephpshort@gmail.com>
2016-06-26 22:06:01 -0400

    results from in-class presentation

---
+++ Database and Entity Framework/DatabaseEntityFrameworkDemoApp/Demo.SqlServer/dbo/Tables/BlogPosts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BlogPosts] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Title]       VARCHAR (100)  NOT NULL,
+    [Body]        NVARCHAR (MAX) NOT NULL,
+    [DateCreated] DATETIME2 (2)  CONSTRAINT [DF_BlogPosts_DateCreated] DEFAULT (getutcdate()) NOT NULL,
+    CONSTRAINT [PK_BlogPosts] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
